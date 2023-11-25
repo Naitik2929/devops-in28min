dd30a0245d5d67d18772f0c923c98abb4f21faae
Naitik Patel <naitikpatel107@gmail.com>
2023-11-25 23:21:11 +0530

    list added

---
+++ 03-list-and-sets/main.tf
@@ -0,0 +1,24 @@
+variable "names" {
+  type = list(string)
+  default = ["foo", "bar", "baz"]
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.51.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "my_iam_users" {
+  #count = length(var.names)
+  #name  = var.names[count.index]
+  for_each = toset(var.names)
+  name     = each.value
+}
+

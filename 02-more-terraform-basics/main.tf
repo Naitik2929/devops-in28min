c11f44473dff9160c7ab31a288576074b45494d0
Naitik Patel <naitikpatel107@gmail.com>
2023-11-25 23:00:25 +0530

    vars priority added

--- 02-more-terraform-basics/main.tf
+++ 02-more-terraform-basics/main.tf
@@ -1,7 +1,13 @@
+variable "iam_user_name_prefix" {
+  type    = string
+  default = "my_iam_users"
+}
+
+
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.51.0"
     }
   }
@@ -11,7 +17,7 @@ provider "aws" {
 }
 
 resource "aws_iam_user" "my_iam_users" {
-    count = 3
-    name = "my_iam_users_${count.index}"
+  count = 3
+  name  = "${var.iam_user_name_prefix}${count.index}"
 }
 

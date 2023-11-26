d65505cdf42e60beeaf0ddfaccba8dd011a6aef8
Naitik Patel <naitikpatel107@gmail.com>
2023-11-26 23:02:49 +0530

    Add default VPC and subnet data sources, and use them in security group and instance resources

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -16,9 +16,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_default_vpc" "default" {}
+data "aws_subnet_ids" "default_subnet" {
+  vpc_id = aws_default_vpc.default.id
+} 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners     = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+}
+data "aws_ami_ids" "amazon_linux_ids" {
+  owners = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+}
+
 resource "aws_security_group" "http_server_sg" {
   name   = "http_server_sg"
-  vpc_id = "vpc-09b5a163cfd065a91"
+  vpc_id = aws_default_vpc.default.id
 
   ingress {
     from_port   = 80
@@ -47,11 +67,11 @@ resource "aws_security_group" "http_server_sg" {
 }
 
 resource "aws_instance" "http_server" {
-  ami                    = "ami-0230bd60aa48260c6"
+  ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
   key_name               = "default-ec2"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
-  subnet_id              = "subnet-0d29ca700344c8541"
+  subnet_id              = tolist(data.aws_subnet_ids.default_subnet.ids)[0]
 
   connection {
     type        = "ssh"

d925410dd01419b26d5fafa1dc8cf67aa74eefbf
Naitik Patel <naitikpatel107@gmail.com>
2023-11-26 21:10:52 +0530

    Add EC2 instance and security group configuration

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -1,7 +1,7 @@
-# This Terraform configuration creates an AWS security group for an HTTP server.
-# The security group allows incoming traffic on port 80 (TCP) and port 22 (TCP) from any IP address (0.0.0.0/0),
-# and allows all outgoing traffic.
-# Replace the vpc_id with the ID of your VPC.
+# This Terraform configuration creates an EC2 instance and a security group in AWS.
+# The EC2 instance is provisioned with an Apache web server and a simple HTML page.
+# The security group allows inbound traffic on ports 80 (HTTP) and 22 (SSH) from any IP address.
+# The EC2 instance is launched in the us-east-1 region, using the specified AMI, instance type, and key pair.
 
 terraform {
   required_providers {
@@ -40,8 +40,31 @@ resource "aws_security_group" "http_server_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   tags = {
     name = "http_server_sg"
+  }
+}
+
+resource "aws_instance" "http_server" {
+  ami                    = "ami-0230bd60aa48260c6"
+  instance_type          = "t2.micro"
+  key_name               = "default-ec2"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id              = "subnet-0d29ca700344c8541"
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("D:\\Code\\Credential\\default-ec2.pem")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Welcome to in28minutes - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
+    ]
   }
 }
